e0e155d3ba22214a29d272f36459c8029935090c
Willem Thiart <himself@willemthiart.com>
2014-05-27 16:54:45 +0900

    Refactor project

--- quadratic_probing_hashmap.h
+++ quadratic_probing_hashmap.h
@@ -1,20 +1,14 @@
-typedef unsigned long (
-    *func_longhash_f
-)    (
-    const void *
-);
+#ifndef QUADRATIC_PROBING_HASHMAP_H
+#define QUADRATIC_PROBING_HASHMAP_H
 
-typedef long (
-    *func_longcmp_f
-)    (
-    const void *,
-    const void *
-);
+typedef unsigned long (*func_longhash_f) (const void *);
+
+typedef long (*func_longcmp_f) (const void *, const void *);
 
 typedef struct
 {
-    void *key;                  // the refrence 
-    void *val;                  // the data
+    void *key;
+    void *val;
 } hash_entry_t;
 
 typedef struct
@@ -41,51 +35,77 @@ hashmapq_t *hashmapq_new(
     unsigned int initial_capacity
 );
 
+/**
+ * @return number of items within hash */
 int hashmapq_count(const hashmapq_t * hmap);
 
 int hashmapq_size(
     hashmapq_t * hmap
 );
 
+/**
+ * Empty this hash. */
 void hashmapq_clear(
     hashmapq_t * hmap
 );
 
+/**
+ * Free all the memory related to this hash. */
 void hashmapq_free(
     hashmapq_t * hmap
 );
 
+/**
+ * Free all the memory related to this hash.
+ * This includes the actual h itself. */
 void hashmapq_freeall(
     hashmapq_t * hmap
 );
 
+/**
+ * Get this key's value.
+ * @return key's item, otherwise NULL */
 void *hashmapq_get(
     hashmapq_t * hmap,
     const void *key
 );
 
+/**
+ * Is this key inside this map?
+ * @return 1 if key is in hash, otherwise 0 */
 int hashmapq_contains_key(
     hashmapq_t * hmap,
     const void *key
 );
 
+/**
+ * Remove the value refrenced by this key from the hash. */
 void hashmapq_remove_entry(
     hashmapq_t * hmap,
     hash_entry_t * entry,
     const void *key
 );
 
+/**
+ * Remove this key and value from the map.
+ * @return value of key, or NULL on failure */
 void *hashmapq_remove(
     hashmapq_t * hmap,
     const void *key
 );
 
+/**
+ * Associate key with val.
+ * Does not insert key if an equal key exists.
+ * @return previous associated val; otherwise NULL */
 void *hashmapq_put(
     hashmapq_t * hmap,
     void *key,
     void *val
 );
 
+/**
+ * Put this key/value entry into the hash */
 void hashmapq_put_entry(
     hashmapq_t * hmap,
     hash_entry_t * entry
@@ -104,18 +124,31 @@ int hashmapq_iterator_has_next(
     hashmapq_iterator_t * iter
 );
 
+/**
+ * Iterate to the next item on a hash iterator
+ * @return next item key from iterator */
 void *hashmapq_iterator_next(
     hashmapq_t * hmap,
     hashmapq_iterator_t * iter
 );
 
+/**
+ * Iterate to the next item on a hash iterator
+ * @return next item value from iterator */
 void *hashmapq_iterator_next_value(
     hashmapq_t * hmap,
     hashmapq_iterator_t * iter);
 
+/**
+ * Initialise a new hash iterator over this hash
+ * It is safe to remove items while iterating.  */
 void hashmapq_iterator(
     hashmapq_t * hmap,
     hashmapq_iterator_t * iter
 );
 
+/**
+ * Increase hash capacity. */
 void hashmapq_increase_capacity(hashmapq_t * hmap);
+
+#endif /* QUADRATIC_PROBING_HASHMAP_H */
